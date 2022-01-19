2237e61734ec624d53f228fbfece3850c795515a
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-19 20:10:51 +0330

    App changed to class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Product from './components/Product/Product';
-const App = () => {
-    return (  
-        <>
+import Product from "./components/Product/Product";
+import { Component } from "react/cjs/react.development";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
         <h2>shopping app</h2>
-        <Product name="react" price="12$"/>
-        <Product name="vue" price="10$"/>
-        <Product name="angular" price="8$"/>
-        </>
+        <Product name="react" price="12$" />
+        <Product name="vue" price="10$" />
+        <Product name="angular" price="8$" />
+      </>
     );
+  }
 }
- 
+
 export default App;
