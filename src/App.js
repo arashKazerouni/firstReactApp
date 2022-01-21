2328dd45ccdd6047943afe3264647299f91e63fe
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-22 00:26:20 +0330

    Products added to ProductList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Product from "./components/Product/Product";
 import { useState } from "react";
+import ProductList from "./ProductList";
 import styles from "./App.module.css"
 const App = () => {
   const [products, setProduct] = useState([
@@ -8,20 +8,12 @@ const App = () => {
     { name: "angular", price: "80$" ,id: 3},
   ]);
 
-  const changeHandler = () => {
-    setProduct([
-      { name: "react", price: "50$" },
-      { name: "vuejs", price: "45$" },
-      { name: "angular", price: "40$" },
-    ]);
-  };
+ 
   return (
     <div className={styles.container}>
        <h2>shopping app</h2>
-      {products.map((product) => {
-        return <Product name={product.name} price={product.price} key={product.id} />;
-      })}
-      <button onClick={changeHandler}>Change Prices</button>
+      <ProductList />
+      
     </div>
   );
 };
---
+++ src/ProductList.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { useState } from "react";
+import Product from "./components/Product/Product";
+const ProductList = () => {
+  const [products, setProduct] = useState([
+    { name: "react", price: "100$", id: 1 },
+    { name: "vuejs", price: "90$", id: 2 },
+    { name: "angular", price: "80$", id: 3 },
+  ]);
+
+
+  return (
+    <>
+      {products.map((product) => {
+        return (
+          <Product
+            name={product.name}
+            price={product.price}
+            key={product.id}
+           
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default ProductList;
