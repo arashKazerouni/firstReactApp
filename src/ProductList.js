3be1215f8cbf33c7979bc3d584d66c426a1bee85
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-23 00:12:27 +0330

    Increment Handler added

--- src/ProductList.js
+++ src/ProductList.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 import Product from "./components/Product/Product";
 const ProductList = () => {
   const [products, setProduct] = useState([
-    { name: "react", price: "100$", id: 1, quantity:1 },
-    { name: "vuejs", price: "90$", id: 2, quantity:1 },
-    { name: "angular", price: "80$", id: 3, quantity:1 },
+    { name: "react", price: "100$", id: 1, quantity: 1 },
+    { name: "vuejs", price: "90$", id: 2, quantity: 2 },
+    { name: "angular", price: "80$", id: 3, quantity: 3 },
   ]);
 
   const removeHandler = (id) => {
@@ -13,16 +13,24 @@ const ProductList = () => {
     setProduct(filteredProduct);
   };
 
+  const incrementHandler = (id) => {
+    const productsClone = [...products];
+    const selectedItem = productsClone.find((p) => p.id === id);
+    selectedItem.quantity++;
+    setProduct(productsClone)
+  
+
+  };
+
   return (
     <>
       {products.map((product) => {
         return (
           <Product
-            name={product.name}
-            price={product.price}
-            quantity={product.quantity}
+            product={product}
             key={product.id}
             onDelete={() => removeHandler(product.id)}
+            onIncrement={() => incrementHandler(product.id)}
           />
         );
       })}
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,10 +4,10 @@ import styles from "./product.module.css";
 const Product = (props) => {
   return (
     <div className={styles.product}>
-      <p>name : {props.name}</p>
-      <p>price : {props.price}</p>
-      <p className={styles.quantity}>{props.quantity}</p>
-      <button className={styles.increment}>increment</button>
+      <p>name : {props.product.name}</p>
+      <p>price : {props.product.price}</p>
+      <span className={styles.quantity}>{props.product.quantity}</span>
+      <button onClick={props.onIncrement} className={styles.increment}>increment</button>
       <button onClick={props.onDelete}>delete</button>
     </div>
   );
