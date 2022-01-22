2de1250b0c70d2bf2eb296ed0bc37335728c3d1a
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-22 18:44:13 +0330

    Extra code removed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import { useState } from "react";
 import ProductList from "./ProductList";
 import styles from "./App.module.css"
 const App = () => {
-  const [products, setProduct] = useState([
-    { name: "react", price: "100$", id: 1 },
-    { name: "vuejs", price: "90$" ,id: 2},
-    { name: "angular", price: "80$" ,id: 3},
-  ]);
+
 
  
   return (
     <div className={styles.container}>
-       <h2>shopping app</h2>
+       <h1>shopping app</h1>
       <ProductList />
       
     </div>
