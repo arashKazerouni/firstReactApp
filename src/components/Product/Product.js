10c3432a78ca49fe1df87b8223bd472a25fc4a35
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-22 18:45:01 +0330

    increment button and quantity addded

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import styles from "./product.module.css";
 
-
-
 const Product = (props) => {
   return (
     <div className={styles.product}>
       <p>name : {props.name}</p>
       <p>price : {props.price}</p>
+      <p className={styles.quantity}>{props.quantity}</p>
+      <button className={styles.increment}>increment</button>
       <button onClick={props.onDelete}>delete</button>
     </div>
   );
