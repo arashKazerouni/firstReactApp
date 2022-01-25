bdd415555cf35f4532d4701f1eac744c763a60a4
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-25 21:14:01 +0330

    Navbar changed to functional component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BsFillBasket2Fill } from "react-icons/bs";
-import styles from "./Navbar.module.css"
-class Navbar extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <section>
-                <nav className={styles.navbar}>
-                <BsFillBasket2Fill className={styles.icon} />
-                <span className={styles.counter}>
-                    1
-                </span>
-                </nav>
-            </section>
-        );
-    }
-}
- 
+import styles from "./Navbar.module.css";
+const Navbar = () => {
+  return (
+    <section>
+      <nav className={styles.navbar}>
+        <BsFillBasket2Fill className={styles.icon} />
+        <span className={styles.counter}>1</span>
+      </nav>
+    </section>
+  );
+};
+
 export default Navbar;
