b6599ea186c0a9f8dd347b64aa73d09ffecef228
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-26 00:29:44 +0330

    no product message got some styles

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { useState } from "react";
 import Product from "./components/Product/Product";
+import styles from "./components/Product/product.module.css"
 const ProductList = (props) => {
   const renderProduct = () => {
-    if (props.products.length === 0) return <div>there is no product for you</div>;
+    if (props.products.length === 0) return <div className={styles.noProduct}>there is no product for you</div>;
     return (
       <div>
         {props.products.map((product) => {
