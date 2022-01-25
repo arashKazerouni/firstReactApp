8d447f5e734686efa2307930d49ca2efbc4fc6d0
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-25 20:00:37 +0330

    navbar added(styles only)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import ProductList from "./ProductList";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
-
   return (
     <div className={styles.container}>
-       <h1>shopping app</h1>
+      <h1>shopping app</h1>
+      <Navbar />
       <ProductList />
-      
     </div>
   );
 };
