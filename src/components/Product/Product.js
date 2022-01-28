ba790c35f031227322a60ad492fe9e32dd9afb4d
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-01-28 13:05:38 +0330

    product.js changed to class component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import styles from "./product.module.css";
 import { BiTrash } from "react-icons/bi";
-import { render } from "@testing-library/react";
+import { Component } from "react/cjs/react.development";
 
-const Product = ({onChange,onIncrement,onDecrement,product,onDelete}) => {
-  console.log("Product.js redner");
-  return (
-    <div className={styles.product}>
+class Product extends Component {
+  render() { 
+    console.log("Product.js redner");
+    const {onChange,onIncrement,onDecrement,product,onDelete} = this.props; 
+    return (
+      <div className={styles.product}>
       <p>name : {product.name} course</p>
       <p>price : {product.price}</p>
       <span className={styles.quantity}>{product.quantity}</span>
@@ -17,7 +19,27 @@ const Product = ({onChange,onIncrement,onDecrement,product,onDelete}) => {
       <button onClick={onIncrement} className={styles.increment}>+</button>
       <button onClick={onDelete}>delete</button>
     </div>
-  );
-};
+    );
+  }
+}
 
 export default Product;
+
+// const Product = ({onChange,onIncrement,onDecrement,product,onDelete}) => {
+//   console.log("Product.js redner");
+//   return (
+//     <div className={styles.product}>
+//       <p>name : {product.name} course</p>
+//       <p>price : {product.price}</p>
+//       <span className={styles.quantity}>{product.quantity}</span>
+//       <input type="text" onChange={onChange} value={product.name} />
+//       <button onClick={onDecrement} className={product.quantity <2 ? styles.red:undefined}>
+//         {product.quantity > 1 ? "-" : <BiTrash/>}
+//       </button>
+//       <button onClick={onIncrement} className={styles.increment}>+</button>
+//       <button onClick={onDelete}>delete</button>
+//     </div>
+//   );
+// };
+
+// export default Product;
