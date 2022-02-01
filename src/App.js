38a8807b5f1caec2016a33c991f72bdb8a541217
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-02-01 14:55:44 +0330

    mistake in decrementHandler Fixed

--- src/App.js
+++ src/App.js
@@ -38,10 +38,16 @@ class App extends Component {
   decrementHandler = (id) => {
     const index = this.state.products.findIndex((item) => item.id === id);
     const product = { ...this.state.products[index] };
-    product.quantity--;
-    const products = [...this.state.products];
-    products[index] = product;
-    this.setState({ products });
+    if (product.quantity === 1) {
+      const filteredProducts = this.state.products.filter((p) => p.id !== id);
+      this.setState({ products: filteredProducts });
+    } else {
+      const products = [...this.state.products ];
+      product.quantity--;
+      products[index] = product;
+      console.log(products);
+      this.setState({ products });
+    }
   };
 
   changeHandler = (event, id) => {
@@ -50,7 +56,7 @@ class App extends Component {
     product.name = event.target.value;
     const products = [...this.state.products];
     products[index].name = product.name;
-    this.setState({products}) 
+    this.setState({ products });
   };
 
   componentDidMount() {
