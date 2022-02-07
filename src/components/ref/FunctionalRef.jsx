69d149fae721f3e7ca869c5f1faefa12cfebecfb
arashKazerouni <94610980+arashKazerouni@users.noreply.github.com>
2022-02-07 10:38:01 +0330

    store previous state with useRef

--- src/components/ref/FunctionalRef.jsx
+++ src/components/ref/FunctionalRef.jsx
@@ -1,10 +1,38 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 const FunctinalRef = () => {
-  const inputRef = useRef();
-  useEffect(()=>{
-      inputRef.current.focus();
-  },[])
-  return <input type="text" ref={inputRef} />;
+  const [input, setInput] = useState("");
+  const [count, setCount] = useState(0);
+  const prevInput = useRef();
+  const prevCount = useRef();
+  useEffect(() => {
+    prevInput.current = input;
+  }, [input]);
+
+  useEffect(() => {
+    prevCount.current = count;
+  }, [count]);
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <p>
+        {" "}
+        your name is {input} and it was {prevInput.current}
+      </p>
+      <br />
+
+      <button onClick={() => setCount(Math.ceil(Math.random() * 100))}>
+        generate random
+      </button>
+      <p>count : {count}</p>
+      <p>previous count : {prevCount.current}</p>
+    </div>
+  );
 };
 
 export default FunctinalRef;
